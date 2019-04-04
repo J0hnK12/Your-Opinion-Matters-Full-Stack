7d153a5627e670bc6dd3a6b309f8f40413e18ea2
J0hnK12 <43782002+J0hnK12@users.noreply.github.com>
2019-04-04 08:13:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-//variables
+//readFile
 let questionOne = [];
 let questionTwo = [];
 let questionThree = [];

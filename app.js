57a0c16cf4869b8133c4beecc6710aee901c3515
J0hnK12 <43782002+J0hnK12@users.noreply.github.com>
2019-04-10 08:40:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-//readFile
+//question empty arrays
 let questionOne = [];
 let questionTwo = [];
 let questionThree = [];
@@ -13,6 +13,8 @@ let questionSeven = [];
 let questionEight = [];
 let questionNine = [];
 let questionTen = [];
+
+//all the choices
 let choice1 = 0;        //a
 let choice2 = 0;        //b
 let choice3 = 0;        //c
@@ -54,6 +56,7 @@ let choice38 = 0;       //am
 let choice39 = 0;       //an
 let choice40 = 0;       //ao
 
+//readfile
 fs.readFile('questionOne.json', async (err, data) => {
     questionOne = await JSON.parse(data); 
 });
@@ -113,21 +116,29 @@ app.post('/answer', (req, res) => {
         d: d
     };
     
-    res.send(data); 
+    res.send(data1); 
     if (req.body.question1 == "a") {
         choice1 += 1;
+        //((choice1) / (choice2 + choice3 + choice4)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question1 == "b") {
         choice2 += 1;
+        //((choice2) / (choice1 + choice3 + choice4)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question1 == "c") {
         choice3 += 1;
+        //((choice3) / (choice2 + choice1 + choice4)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question1 == "d") {
         choice4 += 1;
+        //((choice4) / (choice2 + choice3 + choice1)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question1);
@@ -143,21 +154,29 @@ app.post('/answer', (req, res) => {
         h: h
     };
     
-    res.send(data); 
+    res.send(data2); 
     if (req.body.question2 == "e") {
-        choice5 += 1;;
+        choice5 += 1;
+        //((choice5) / (choice6 + choice7 + choice8)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question2 == "f") {
-        choice6 += 1;;
+        choice6 += 1;
+        //((choice6) / (choice5 + choice7 + choice8)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question2 == "g") {
-        choice7 += 1;;
+        choice7 += 1;
+        //((choice7) / (choice6 + choice5 + choice8)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question2 == "h") {
-        choice8 += 1;;
+        choice8 += 1;
+        //((choice8) / (choice6 + choice7 + choice5)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question2);
@@ -173,21 +192,29 @@ app.post('/answer', (req, res) => {
         l: l
     };
     
-    res.send(data); 
+    res.send(data3); 
     if (req.body.question3 == "i") {
         choice9 += 1;
+        //((choice9) / (choice10 + choice11 + choice12)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question3 == "j") {
         choice10 += 1;
+        //((choice10) / (choice9 + choice11 + choice12)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question3 == "k") {
         choice11 += 1;
+        //((choice11) / (choice10 + choice9 + choice12)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question3 == "l") {
         choice12 += 1;
+        //((choice12) / (choice10 + choice11 + choice9)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question3);
@@ -203,21 +230,29 @@ app.post('/answer', (req, res) => {
         p: p
     };
     
-    res.send(data); 
+    res.send(data4); 
     if (req.body.question4 == "m") {
         choice13 += 1;
+        //((choice13) / (choice14 + choice15 + choice16)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question4 == "n") {
         choice14 += 1;
+        //((choice14) / (choice13 + choice15 + choice16)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question4 == "o") {
         choice15 += 1;
+        //((choice15) / (choice14 + choice13 + choice16)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question4 == "p") {
         choice16 += 1;
+        //((choice16) / (choice14 + choice15 + choice13)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question4);
@@ -233,21 +268,29 @@ app.post('/answer', (req, res) => {
         t: t
     };
     
-    res.send(data); 
+    res.send(data5); 
     if (req.body.question5 == "q") {
         choice17 += 1;
+        //((choice17) / (choice18 + choice19 + choice20)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question5 == "r") {
         choice18 += 1;
+        //((choice18) / (choice17 + choice19 + choice20)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question5 == "s") {
         choice19 += 1;
+        //((choice19) / (choice18 + choice17 + choice20)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question5 == "t") {
         choice20 += 1;
+        //((choice20) / (choice18 + choice19 + choice17)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question5);
@@ -263,21 +306,29 @@ app.post('/answer', (req, res) => {
         x: x
     };
     
-    res.send(data); 
+    res.send(data6); 
     if (req.body.question6 == "u") {
         choice21 += 1;
+        //((choice21) / (choice22 + choice23 + choice24)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question6 == "v") {
         choice22 += 1;
+        //((choice22) / (choice21 + choice23 + choice24)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question6 == "w") {
         choice23 += 1;
+        //((choice23) / (choice22 + choice21 + choice24)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question6 == "x") {
         choice24 += 1;
+        //((choice24) / (choice22 + choice23 + choice21)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question6);
@@ -293,21 +344,29 @@ app.post('/answer', (req, res) => {
         ac: ac
     };
     
-    res.send(data); 
+    res.send(data7); 
     if (req.body.question7 == "y") {
         choice25 += 1;
+        //((choice25) / (choice26 + choice27 + choice28)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question7 == "z") {
         choice26 += 1;
+        //((choice26) / (choice25 + choice27 + choice28)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question7 == "ab") {
         choice27 += 1;
+        //((choice27) / (choice26 + choice25 + choice28)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question7 == "ac") {
         choice28 += 1;
+        //((choice28) / (choice26 + choice27 + choice25)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question7);
@@ -323,21 +382,29 @@ app.post('/answer', (req, res) => {
         ag: ag
     };
     
-    res.send(data); 
+    res.send(data8); 
     if (req.body.question8 == "ad") {
         choice29 += 1;
+        //((choice29) / (choice30 + choice31 + choice32)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question8 == "ae") {
         choice30 += 1;
+        //((choice30) / (choice29 + choice31 + choice32)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question8 == "af") {
         choice31 += 1;
+        //((choice31) / (choice30 + choice29 + choice32)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question8 == "ag") {
         choice32 += 1;
+        //((choice32) / (choice30 + choice31 + choice29)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question8);
@@ -353,21 +420,29 @@ app.post('/answer', (req, res) => {
         ak: ak
     };
     
-    res.send(data); 
+    res.send(data9); 
     if (req.body.question9 == "ah") {
         choice33 += 1;
+        //((choice33) / (choice34 + choice35 + choice36)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question9 == "ai") {
         choice34 += 1;
+        //((choice34) / (choice33 + choice35 + choice36)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question9 == "aj") {
         choice35 += 1;
+        //((choice35) / (choice34 + choice33 + choice36)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question9 == "ak") {
         choice36 += 1;
+        //((choice36) / (choice34 + choice35 + choice33)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question9);
@@ -383,21 +458,29 @@ app.post('/answer', (req, res) => {
         ao: ao
     };
     
-    res.send(data); 
+    res.send(data10); 
     if (req.body.question10 == "al") {
         choice37 += 1;
+        //((choice37) / (choice38 + choice39 + choice40)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question10 == "am") {
         choice38 += 1;
+        //((choice38) / (choice37 + choice39 + choice40)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question10 == "an") {
         choice39 += 1;
+        //((choice39) / (choice38 + choice37 + choice40)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     if (req.body.question10 == "ao") {
         choice40 += 1;
+        //((choice40) / (choice38 + choice39 + choice37)) * 100
+        //get the percentage to show up after the user clicks the button
     }
     
     console.log(req.body.question10);
